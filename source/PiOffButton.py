f8840a835bc1f43f2c4fe9d88cdfba18a20df085
ACarling <aidanc@internode.on.net>
2019-10-03 16:03:00 +0930

    Update PiOffButton.py

--- source/PiOffButton.py
+++ source/PiOffButton.py
@@ -1,14 +1,18 @@
-from gpiozero
+import RPi.GPIO as GPIO
 import time
 import os
 import gpioModule as relay
 
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pulll_up_down=GPIO.PUD_UP)
+
 #wiring goes from gpio 6 - pin 22; to gnd - pin 20
-stopButton = gpiozero.button(6)
 
 while True:
-    if stopButton.is_pressed:
+    if GPIO.input(10) == GPIO.LOW:
         relay.allFlash()
-        if stopButton.is_pressed:
+        if GPIO.input(10) == GPIO.LOW:
             os.system("shutdown now -h")
     time.sleep(1)
